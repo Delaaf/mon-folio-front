c9e1e410d9933486f46d5acbb10aea890a849dd1
NoelDelafosse <delafossekouakou863@gmail.com>
2026-05-01 11:29:37 +0000

    correction de la responsivité de la table des projets dans gstion projets 2

--- src/pages/backoffice/GestionProjets.jsx
+++ src/pages/backoffice/GestionProjets.jsx
@@ -187,8 +187,8 @@ export default function GestionProjets() {
             <h1 className={s.pageTitle}>Gestion des <em>projets</em></h1>
             <p className={s.pageSubtitle}>{projects?.length ?? 0} projets · {published} publiés</p>
           </div>
-          <Space>
-            <Button icon={<ReloadOutlined />} onClick={fetchProjects} loading={loading} />
+          <Space className={s.btnDiv}>
+            <Button icon={<ReloadOutlined />} onClick={fetchProjects} loading={loading} className={s.reloadIcon}/>
             <Button onClick={() => setCatModal(true)}>Nouvelle catégorie</Button>
             <Button type="primary" icon={<PlusOutlined />} onClick={openCreate}>Nouveau projet</Button>
           </Space>
